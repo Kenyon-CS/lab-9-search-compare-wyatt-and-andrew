abd82ff25361368ffd5029355c5787e5a3fdfe54
Andrew Pilat <pilat1@kenyon.edu>
2024-11-15 16:18:38 -0500

    Wrote all the functions

--- main.cpp
+++ main.cpp
@@ -8,9 +8,9 @@ using namespace std;
 
 // Sequential search
 int seq_search(vector<int> &keys, int key, int &numCmp) {
-    for(int i = 0; i < keys.size(); i++){
+    for(int i = 0; i < keys.size(); i++){ //Iterate over the keys to find the match
         numCmp ++;
-        if(keys[i] == key){
+        if(keys[i] == key){ 
             return i;
         }
     }
@@ -19,7 +19,7 @@ int seq_search(vector<int> &keys, int key, int &numCmp) {
 
 // Sequential search with start and stop range
 int seq_search(vector<int> &keys, int key, int start, int stop, int &numCmp) {
-for(int i = start; i < stop; i++){
+for(int i = start; i < stop; i++){ //Iterate over the keys with a certain start and stop position
         numCmp ++;
         if(keys[i] == key){
             return i;
@@ -32,34 +32,67 @@ for(int i = start; i < stop; i++){
 
 // Binary search
 int bin_search(vector<int> &keys, int key, int &numCmp) {
-
-numCmp++;
-
-int midpoint = (keys.size()/2);
-vector<int> Half;
-
-if(key > keys[midpoint]){
-    copy(keys.begin()+midpoint
-    +1, keys.end(), Half.begin());
-    bin_search(Half, key, numCmp);
-}
-else if(key < keys[midpoint]){
-    copy(keys.begin(), keys.begin()+midpoint, Half.begin());
-    bin_search(Half, key, numCmp);
-}
-else if(key == keys[midpoint]){
+    int first = 0;
+    bool found =false;
+    int last = keys.size()-1;
+    int midpoint = (first+last)/2;
+while(first <= last && !found){
+    numCmp++;
+    if( ((midpoint == 0) && key != keys[0]) ||  ((midpoint == keys.size()-1) && key != keys[keys.size()-1])){
+        midpoint = -1;
+    }
+    else if(key > keys[midpoint]){
+    first = midpoint +1;
+    midpoint = (first +last)/2;
+} else if(key < keys[midpoint]){
+    last = midpoint -1;
+    midpoint = (first +last)/2;
+} else if (key == keys[midpoint]){
     return midpoint;
+} 
 }
+if (found){
+return midpoint;}
 else{
     return -1;
 }
-
-
-
 }
+
 
 // Enhanced binary search: switches to sequential search if remaining range is < 15
 int bin2_search(vector<int> &keys, int key, int &numCmp) {
+    bool found = false;
+    int midpoint;
+    int first = 0;
+    int last = keys.size() -1;
+    midpoint = (first +last)/2;
+while(first <= last && !found){
+    numCmp++;
+    if(key > keys[midpoint]){
+        if(last-first < 15){
+            numCmp = numCmp -1;
+        midpoint = seq_search(keys, key, first, last, numCmp);
+        return midpoint;
+    }
+    first = midpoint +1;
+    midpoint = (first +last)/2;
+} else if(key < keys[midpoint]){
+     if(midpoint < 15){
+        numCmp = numCmp -1;
+        midpoint = seq_search(keys, key, first, last, numCmp);
+        return midpoint;
+    }
+    last = midpoint -1;
+    midpoint = (first +last)/2;
+} else if (key == keys[midpoint]){
+    return midpoint;
+} 
+}
+if(found){
+return midpoint;
+} else{
+    return -1;
+}
 
 }
 
@@ -71,7 +104,7 @@ int main() {
     // Create a vector of 100,000 integers
     vector<int> numVector;
     for(int i = 0; i < 100000; i++){
-        numVector.push_back(rand() % 100000000);
+        numVector.push_back(rand() % 10000000);
     }
 
     // Sort the vector using STL sort
@@ -86,24 +119,48 @@ int main() {
     // Generate 50 random numbers (may or may not be in the array)
     vector<int> random50_new;
     for(int i = 0; i < 50; i++){
-        random50_new.push_back(rand() % 100000000);
+        random50_new.push_back(rand() % 10000000);
     }
+
+    //vector<int> testVector = {3,4,5,6,7,8,9,10,33,34,50};
+    //int numberOfComparisonsSimpleSearch = 0;
+    //int keyPosition = bin2_search(testVector, 3, numberOfComparisonsSimpleSearch);
+   // cout << "Found: " << keyPosition << " Comparisons: " << numberOfComparisonsSimpleSearch;
+
 
     // Test each search algorithm, output results
-        int numberOfComparisonsSimpleSearch = 0;
-        int numberOfComparisonsBinSearch = 0;
-        int numberOfComparisonsEnhancedSearch = 0;
+    int keyPositionSequential, keyPositionBin, keyPositionBinEnhanced;
+    int numberOfComparisonsSimpleSearch;
+    int numberOfComparisonsBinSearch;
+    int numberOfComparisonsEnhancedSearch;
+    cout <<"-----------------------------------------------------------------------" << endl;
+    cout << "---------------------------------In Data------------------------------" << endl;
+    cout <<"-----------------------------------------------------------------------" << endl;
+    cout << "Search" << "\t" << "Found" << "\t" << "Seq Search" << "\t" << "Bin search" << "\t" << "Enhanced Search" << endl;
+     for(int i = 0; i < random50_new.size(); i++){
+        numberOfComparisonsSimpleSearch = 0;
+        numberOfComparisonsBinSearch = 0;
+        numberOfComparisonsEnhancedSearch = 0;
+      keyPositionSequential = seq_search(numVector, random50_in_data[i], numberOfComparisonsSimpleSearch);
+      keyPositionBin = bin_search(numVector, random50_in_data[i], numberOfComparisonsBinSearch);
+      keyPositionBinEnhanced = bin2_search(numVector, random50_in_data[i], numberOfComparisonsEnhancedSearch);
+      cout << random50_in_data[i] << "\t" << keyPositionSequential << "\t" << numberOfComparisonsSimpleSearch 
+      << "\t" << numberOfComparisonsBinSearch << "\t" << numberOfComparisonsEnhancedSearch << endl;
+   }
+     cout <<"-----------------------------------------------------------------------" << endl;
+     cout << "---------------------------------Not necessarily in Data------------------------------" << endl;
+    cout <<"-----------------------------------------------------------------------" << endl;
+        cout << "Search" << "\t" << "Found" << "\t" << "Seq Search" << "\t" << "Bin search" << "\t" << "Enhanced Search" << endl;
     for(int i = 0; i < random50_new.size(); i++){
-        int keyPosition = seq_search(numVector, random50_in_data[i], numberOfComparisonsSimpleSearch);
-        int keyPosition = bin_search(numVector, random50_in_data[i], numberOfComparisonsBinSearch);
-        int keyPosition = bin2_search(numVector, random50_in_data[i], numberOfComparisonsEnhancedSearch);
-        cout << random50_in_data[i] << "\t" << keyPosition << "\t" << numberOfComparisonsSimpleSearch 
-        << "\t" << numberOfComparisonsBinSearch << "\t" << numberOfComparisonsEnhancedSearch;
-    }
-
-
-
-
+        numberOfComparisonsSimpleSearch = 0;
+        numberOfComparisonsBinSearch = 0;
+        numberOfComparisonsEnhancedSearch = 0;
+      keyPositionSequential = seq_search(numVector, random50_new[i], numberOfComparisonsSimpleSearch);
+      keyPositionBin = bin_search(numVector, random50_new[i], numberOfComparisonsBinSearch);
+      keyPositionBinEnhanced = bin2_search(numVector, random50_new[i], numberOfComparisonsEnhancedSearch);
+      cout << random50_in_data[i] << "\t" << keyPositionSequential << "\t" << numberOfComparisonsSimpleSearch 
+      << "\t" << numberOfComparisonsBinSearch << "\t" << numberOfComparisonsEnhancedSearch << endl;
+   }
 
 
     return 0;
